5f4d341fdbc70fd86cdb5f0275ba5ab51003717f
Ignacio Santolin <isantolin@users.noreply.github.com>
2019-04-19 07:08:10 -0300

    [19.04] Java updated to 12, Zend Removed, Symfony fixed,

--- ubuntu_postinstall.sh
+++ ubuntu_postinstall.sh
@@ -21,7 +21,7 @@ sudo dpkg --install skypeforlinux-64.deb
 rm skypeforlinux-64.deb
 sudo apt -y install -f
 
-sudo add-apt-repository ppa:webupd8team/java -y
+sudo add-apt-repository ppa:linuxuprising/java -y
 
 sudo apt update
 sudo apt -y upgrade
@@ -35,7 +35,7 @@ sudo depmod -ae && sudo update-initramfs -u
 sudo apt -y install --install-recommends winehq-devel
 sudo apt -y install --install-recommends brasero
 sudo apt -y install virtualbox-6.0
-sudo apt -y install winbind apt-transport-https webmin tasksel ubuntu-restricted-extras build-essential synaptic libdvd-pkg  default-jdk default-jre libreoffice printer-driver-cups-pdf filezilla rabbitvcs-nautilus ffmpeg git oracle-java8-installer oracle-java8-set-default ruby-sass node-less php-codesniffer phpmd composer php-doctrine-orm phing phpunit gfortran cmake npm nodejs qt5-qmake curl zend-framework-bin network-manager-fortisslvpn-gnome network-manager-iodine-gnome network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-ssh-gnome network-manager-strongswan network-manager-vpnc-gnome python3-pip gstreamer1.0-nice gstreamer1.0-omx-generic gstreamer1.0-opencv gstreamer1.0-pipewire gstreamer1.0-pocketsphinx gstreamer1.0-rtsp gstreamer1.0-plugins-bad
+sudo apt -y install winbind apt-transport-https webmin tasksel ubuntu-restricted-extras build-essential synaptic libdvd-pkg  default-jdk default-jre libreoffice printer-driver-cups-pdf filezilla rabbitvcs-nautilus ffmpeg git oracle-java12-installer oracle-java12-set-default ruby-sass node-less php-codesniffer phpmd composer php-doctrine-orm phing phpunit gfortran cmake npm nodejs qt5-qmake curl network-manager-fortisslvpn-gnome network-manager-iodine-gnome network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-ssh-gnome network-manager-strongswan network-manager-vpnc-gnome python3-pip gstreamer1.0-nice gstreamer1.0-omx-generic gstreamer1.0-opencv gstreamer1.0-pipewire gstreamer1.0-pocketsphinx gstreamer1.0-rtsp gstreamer1.0-plugins-bad
 
 sudo usermod -a -G vboxusers $USER
 
@@ -51,22 +51,22 @@ read password
 
 sudo mysql -u root -p -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password';"
 
-curl http://get.sensiolabs.org/php-cs-fixer.phar -o php-cs-fixer
+curl https://cs.symfony.com/download/php-cs-fixer-v2.phar -o php-cs-fixer
 sudo chmod a+x php-cs-fixer
 sudo mv php-cs-fixer /usr/bin/php-cs-fixer
 
-sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
-sudo chmod a+x /usr/local/bin/symfony
+wget https://get.symfony.com/cli/installer -O - | bash
+sudo mv /home/$USER/.symfony/bin/symfony /usr/local/bin/symfony
 
 wget https://github.com/atoum/atoum/releases/download/3.3.0/atoum.phar
 sudo chmod a+x atoum.phar
 sudo mv atoum.phar /usr/bin/atoum
 
-wget https://github.com/nette/tester/releases/download/v2.1.0/tester.phar 
+wget https://github.com/nette/tester/releases/download/v2.2.0/tester.phar 
 sudo chmod a+x tester.phar
 sudo mv tester.phar /usr/bin/tester
 
-wget https://github.com/phpstan/phpstan/releases/download/0.10.6/phpstan.phar
+wget https://github.com/phpstan/phpstan/releases/download/0.11.5/phpstan.phar
 sudo chmod a+x phpstan.phar
 sudo mv phpstan.phar /usr/bin/phpstan.phar
 
