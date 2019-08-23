46382ed51daa2669e8ebbb5d817cf2fb959798ce
Ignacio Santolin <isantolin@users.noreply.github.com>
2019-08-23 17:01:23 -0300

    Rearranged items

--- fedora_postinstall.sh
+++ fedora_postinstall.sh
@@ -2,9 +2,12 @@
 # TODO: Instalacion NVIDIA - https://acrelinux.org/ativando-o-nvidia-optimus-no-fedora/
 #- Refresh de Cache una vez por día
 #- Java
+# General Configs
+sudo echo "blacklist psmouse" | sudo tee /etc/modprobe.d/blacklist.conf
+sudo depmod -ae && sudo dracut -f /boot/initramfs-currentimage
 
+#Repository Add
 dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/$(rpm -E %fedora)/winehq.repo
-dnf install winehq-devel
 
 sudo echo "[Webmin]
 name=Webmin Distribution Neutral
@@ -12,27 +15,22 @@ name=Webmin Distribution Neutral
 mirrorlist=https://download.webmin.com/download/yum/mirrorlist
 enabled=1
 gpgkey=http://www.webmin.com/jcameron-key.asc" | sudo tee /etc/yum.repos.d/webmin.repo
-
 dnf config-manager --add-repo /etc/yum.repos.d/webmin.repo
 
-sudo dnf install https://dl.google.com/linux/direct/google-chrome-beta_current_x86_64.rpm
-sudo dnf install https://go.skype.com/skypeforlinux-64.rpm
-sudo dnf install webmin samba-winbind
+# Other repository and external packages install
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm https://download3.ebz.epson.net/dsc/f/03/00/09/76/14/ab1eea2026fa53856550fd626f534a7dcb0e6e42/epson-inkjet-printer-escpr-1.7.2-1lsb3.2.x86_64.rpm https://download3.ebz.epson.net/dsc/f/03/00/09/76/16/7a70c5e4e0f5a9e168c6f0228818a23794bbcb48/epson-printer-utility-1.1.0-1lsb3.2.x86_64.rpm https://go.skype.com/skypeforlinux-64.rpm https://dl.google.com/linux/direct/google-chrome-beta_current_x86_64.rpm
 
-sudo echo "blacklist psmouse" | sudo tee /etc/modprobe.d/blacklist.conf
-sudo depmod -ae && sudo dracut -f /boot/initramfs-currentimage
+# Update to install repository packages
+sudo find /etc/yum.repos.d/*.repo -type f -exec sed -i 's/enabled=0/enabled=1/g' {} \;
+sudo dnf update
+sudo dnf -y install webmin samba-winbind httpd gcc-c++ make winehq-devel nodejs php php-cli php-php-gettext php-mbstring php-mcrypt php-mysqlnd php-pear php-curl php-gd php-xml php-bcmath php-zip mariadb-server cups-pdf cups-lpd
 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-sudo dnf install https://download3.ebz.epson.net/dsc/f/03/00/09/76/14/ab1eea2026fa53856550fd626f534a7dcb0e6e42/epson-inkjet-printer-escpr-1.7.2-1lsb3.2.x86_64.rpm
-sudo dnf install https://download3.ebz.epson.net/dsc/f/03/00/09/76/16/7a70c5e4e0f5a9e168c6f0228818a23794bbcb48/epson-printer-utility-1.1.0-1lsb3.2.x86_64.rpm
-
-sudo dnf -y install httpd
+# Lamp Configuration
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo firewall-cmd --add-service={http,https} --permanent
 sudo firewall-cmd --reload
-sudo dnf -y install php php-cli php-php-gettext php-mbstring php-mcrypt php-mysqlnd php-pear php-curl php-gd php-xml php-bcmath php-zip mariadb-server cups-pdf cups-lpd
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 sudo mysql_secure_installation
@@ -41,9 +39,7 @@ read password
 
 sudo mysql -u root -p -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password';"
 
-sudo find /etc/yum.repos.d/*.repo -type f -exec sed -i 's/enabled=0/enabled=1/g' {} \;
-sudo dnf update
-
+# Web Stuff related to Netbeans
 curl https://cs.symfony.com/download/php-cs-fixer-v2.phar -o php-cs-fixer
 sudo chmod a+x php-cs-fixer
 sudo mv php-cs-fixer /usr/bin/php-cs-fixer
@@ -77,10 +73,6 @@ sudo mv phpunit-skelgen.phar /usr/local/bin/phpunit-skelgen
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
-
-sudo dnf install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
-sudo dnf install -y nodejs
 
 sudo npm install -g npm@latest
 sudo npm install --global gulp grunt karma phonegap bower express-generator cordova
