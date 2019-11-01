f047fe22e2195d9426ad5519c3fd95330f01190a
Ignacio Santolin <ignacio.santolin@gmail.com>
2019-11-01 12:45:04 -0300

    Dbeaver added, minor fixes

--- postinstall_fedora.sh
+++ postinstall_fedora.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 #- Refresh de Cache una vez por día
-#- Java (No funciona Validación)
-#- Simplificar Logica de Java
 #- Agregar Color Remark de Consola (Fedy)
 #- Agregar Netbeans
 
-# Get Arch
-if [[ "$(uname -m)" = "x86_64" ]]; then
-    ARCH="x64"
-else
-    ARCH="x86"
-fi
-
+# System configs
 sudo echo "blacklist psmouse" | sudo tee /etc/modprobe.d/blacklist.conf
 sudo depmod -ae && sudo dracut -f /boot/initramfs-currentimage
-sudo echo "fastestmirror=True" >> /etc/dnf/dnf.conf
-sudo echo "deltarpm=True" >> /etc/dnf/dnf.conf
+sudo echo "fastestmirror=True
+deltarpm=True" >> /etc/dnf/dnf.conf
 sudo echo -e "127.0.0.1\tlocalhost airwave7
 ::1\tlocalhost airwave7" | sudo tee /etc/hosts
 sudo hostnamectl set-hostname airwave7
 
-#Repository Add
+# Repository Add
 dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/$(rpm -E %fedora)/winehq.repo
-
 sudo echo "[Webmin]
 name=Webmin Distribution Neutral
 #baseurl=https://download.webmin.com/download/yum
@@ -32,15 +23,16 @@ gpgkey=http://www.webmin.com/jcameron-key.asc" | sudo tee /etc/yum.repos.d/webmi
 dnf config-manager --add-repo /etc/yum.repos.d/webmin.repo
 
 # Other repository and external packages install
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm  https://go.skype.com/skypeforlinux-64.rpm https://dl.google.com/linux/direct/google-chrome-beta_current_$(uname -m).rpm http://linuxdownload.adobe.com/adobe-release/adobe-release-$(uname -i)-1.0-1.noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm  https://go.skype.com/skypeforlinux-64.rpm https://dl.google.com/linux/direct/google-chrome-beta_current_$(uname -m).rpm http://linuxdownload.adobe.com/adobe-release/adobe-release-$(uname -i)-1.0-1.noarch.rpm https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm
 sudo dnf -y install rpmfusion-free-release-tainted
+
 # Update to install repository packages
 sudo find /etc/yum.repos.d/*.repo -type f -exec sed -i 's/enabled=0/enabled=1/g' {} \;
 sudo dnf -y update
 sudo dnf -y install webmin samba-winbind httpd gcc-c++ make winehq-devel nodejs php php-cli php-php-gettext php-mbstring php-mcrypt php-pgsql php-pear php-curl php-gd php-xml php-bcmath php-zip cups-pdf cups-lpd libdvdcss cabextract lzip p7zip p7zip-plugins unrar flash-plugin alsa-plugins-pulseaudio libcurl flash-player-ppapi lpf-mscore-fonts postgresql-server postgresql-contrib gstreamer1-plugin-openh264 gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-free-fluidsynth gstreamer1-plugins-bad-free-wildmidi gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-entrans gstreamer1-plugins-fc gstreamer1-plugins-good-extras gstreamer1-rtsp-server gstreamer1-vaapi gstreamer1-plugins-ugly xorg-x11-drv-nvidia-390xx akmod-nvidia-390xx xorg-x11-drv-nvidia-390xx-cuda kernel-devel vdpauinfo libva-vdpau-driver libva-utils php-json NetworkManager-fortisslvpn-gnome NetworkManager-iodine-gnome NetworkManager-l2tp-gnome NetworkManager-libreswan-gnome NetworkManager-sstp-gnome NetworkManager-strongswan-gnome epson-inkjet-printer-escpr gstreamer1-svt-vp9 gstreamer1-svt-av1 NetworkManager-ovs
-sudo akmods --force
 
 # Xorg --> Wayland
+sudo akmods --force
 sudo sed -i '/WaylandEnable=false/d' /etc/gdm/custom.conf
 sudo sed -i '/DRIVER==/d' /usr/lib/udev/rules.d/61-gdm.rules
 
@@ -48,7 +40,6 @@ sudo sed -i '/DRIVER==/d' /usr/lib/udev/rules.d/61-gdm.rules
 sudo systemctl enable postgresql
 sudo postgresql-setup --initdb --unit postgresql
 sudo systemctl start postgresql
-
 sudo firewall-cmd --add-service={http,https,postgresql} --permanent
 sudo firewall-cmd --reload
 
@@ -80,7 +71,7 @@ wget https://github.com/nette/tester/releases/download/v2.3.0/tester.phar
 sudo chmod a+x tester.phar
 sudo mv tester.phar /usr/bin/tester
 
-wget https://github.com/phpstan/phpstan/releases/download/0.11.15/phpstan.phar
+wget https://github.com/phpstan/phpstan/releases/download/0.11.19/phpstan.phar
 sudo chmod a+x phpstan.phar
 sudo mv phpstan.phar /usr/bin/phpstan.phar
 
