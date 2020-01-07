3f809c5b6533bc738b9c2812d90bb3b1b09a60bd
Ignacio Santolin <ignacio.santolin@gmail.com>
2020-01-07 19:23:39 -0300

    Updates for Ubuntu 19.10

--- postinstall_ubuntu.sh
+++ postinstall_ubuntu.sh
@@ -11,7 +11,6 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
 wget https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo dpkg --install google-chrome-beta_current_amd64.deb
 rm google-chrome-beta_current_amd64.deb
 
@@ -19,7 +18,6 @@ wget https://repo.skype.com/latest/skypeforlinux-64.deb
 wget -q -O - https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 sudo dpkg --install skypeforlinux-64.deb
 rm skypeforlinux-64.deb
-sudo apt -y install -f
 
 sudo add-apt-repository ppa:linuxuprising/java -y
 
@@ -34,8 +32,8 @@ sudo depmod -ae && sudo update-initramfs -u
 
 sudo apt -y install --install-recommends winehq-devel
 sudo apt -y install --install-recommends brasero
-sudo apt -y install virtualbox-6.0
-sudo apt -y install winbind apt-transport-https webmin tasksel ubuntu-restricted-extras build-essential synaptic libdvd-pkg  default-jdk default-jre libreoffice printer-driver-cups-pdf filezilla rabbitvcs-nautilus ffmpeg git oracle-java12-installer oracle-java12-set-default ruby-sass node-less php-codesniffer phpmd composer php-doctrine-orm phing phpunit gfortran cmake npm nodejs qt5-qmake curl network-manager-fortisslvpn-gnome network-manager-iodine-gnome network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-ssh-gnome network-manager-strongswan network-manager-vpnc-gnome python3-pip gstreamer1.0-nice gstreamer1.0-omx-generic gstreamer1.0-opencv gstreamer1.0-pipewire gstreamer1.0-pocketsphinx gstreamer1.0-rtsp gstreamer1.0-plugins-bad
+sudo apt -y install virtualbox-6.1
+sudo apt -y install winbind apt-transport-https webmin tasksel ubuntu-restricted-extras build-essential synaptic libdvd-pkg  default-jdk default-jre libreoffice printer-driver-cups-pdf filezilla rabbitvcs-nautilus ffmpeg git oracle-java13-installer oracle-java13-set-default ruby-sass node-less php-codesniffer phpmd composer php-doctrine-orm phing phpunit gfortran cmake npm nodejs qt5-qmake curl network-manager-fortisslvpn-gnome network-manager-iodine-gnome network-manager-l2tp-gnome network-manager-openconnect-gnome network-manager-ssh-gnome network-manager-strongswan network-manager-vpnc-gnome python3-pip gstreamer1.0-nice gstreamer1.0-omx-generic gstreamer1.0-opencv gstreamer1.0-pipewire gstreamer1.0-pocketsphinx gstreamer1.0-rtsp gstreamer1.0-plugins-bad
 
 sudo usermod -a -G vboxusers $USER
 
@@ -51,22 +49,33 @@ read password
 
 sudo mysql -u root -p -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password';"
 
+# Web Stuff related to Netbeans
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+wget -O phpunit https://phar.phpunit.de/phpunit-8.phar
+sudo mv phpunit /usr/local/bin/phpunit
+sudo chmod -x /usr/local/bin/phpunit
+
+sudo pear channel-discover pear.phing.info
+sudo pear install phing/phing
+
 curl https://cs.symfony.com/download/php-cs-fixer-v2.phar -o php-cs-fixer
 sudo chmod a+x php-cs-fixer
 sudo mv php-cs-fixer /usr/bin/php-cs-fixer
 
 wget https://get.symfony.com/cli/installer -O - | bash
-sudo mv /home/$USER/.symfony/bin/symfony /usr/local/bin/symfony
+mv /root/.symfony/bin/symfony /usr/local/bin/symfony
 
 wget https://github.com/atoum/atoum/releases/download/3.3.0/atoum.phar
 sudo chmod a+x atoum.phar
 sudo mv atoum.phar /usr/bin/atoum
 
-wget https://github.com/nette/tester/releases/download/v2.2.0/tester.phar 
+wget https://github.com/nette/tester/releases/download/v2.3.1/tester.phar 
 sudo chmod a+x tester.phar
 sudo mv tester.phar /usr/bin/tester
 
-wget https://github.com/phpstan/phpstan/releases/download/0.11.12/phpstan.phar
+wget https://github.com/phpstan/phpstan/releases/download/0.11.19/phpstan.phar
 sudo chmod a+x phpstan.phar
 sudo mv phpstan.phar /usr/bin/phpstan.phar
 
@@ -74,8 +83,10 @@ wget http://codeception.com/codecept.phar
 sudo chmod +x codecept.phar
 sudo mv codecept.phar /usr/bin/codecept.phar
 
-sudo pear channel-discover pear.phpdoc.org
-sudo pear install phpdoc/phpDocumentor
+sudo wget https://pear.phpdoc.org/channel.xml
+sudo pear channel-add ./channel.xml
+sudo pear install phpdocumentor
+sudo rm ./channel.xml
 
 wget https://phar.phpunit.de/phpunit-skelgen.phar
 chmod +x phpunit-skelgen.phar
@@ -85,7 +96,8 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
+sudo pear install PHP_CodeSniffer
+
 sudo npm install -g npm@latest
-sudo npm install --global gulp grunt karma phonegap bower express-generator cordova
-# /usr/share/X11/xorg.conf.d/*
+sudo npm install --global gulp grunt karma phonegap bower express-generator cordova less sass
 sudo pip3 install pip --upgrade
