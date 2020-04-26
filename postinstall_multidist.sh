cdd4523fac4cd451a839fae51c75ead3b35df676
Ignacio Santolin <264640+isantolin@users.noreply.github.com>
2020-04-26 16:42:53 -0300

    Added Scheduler

--- postinstall_multidist.sh
+++ postinstall_multidist.sh
@@ -177,3 +177,26 @@ sudo pear install PHP_CodeSniffer
 sudo npm install -g npm@latest
 sudo npm install --global gulp grunt karma phonegap bower express-generator cordova less sass
 sudo pip3 install pip --upgrade
+
+# IO scheduler udev rules: https://wiki.archlinux.org/index.php/Solid_State_Drives
+# Deadline on SSDs: https://wiki.debian.org/SSDOptimization#Low-Latency_IO-Scheduler
+ssd_scheduler="deadline"
+hdd_scheduler="deadline"
+
+cat <<EOF | tee "/etc/udev/rules.d/60-io_schedulers.rules" > /dev/null 2>&1
+# Set deadline scheduler for non-rotating disks
+ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="$ssd_scheduler"
+# Set deadline scheduler for rotating disks
+ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="$hdd_scheduler"
+EOF
+
+for disk in /sys/block/sd*; do
+    rot="$disk/queue/rotational"
+    sched="$disk/queue/scheduler"
+
+    if [[ $(cat "$rot") -eq 0 ]]; then
+        echo "$ssd_scheduler | tee $sched > /dev/null 2>&1"
+    elif [[ $(cat "$rot") -eq 1 ]]; then
+        echo "$hdd_scheduler | tee $sched > /dev/null 2>&1"
+    fi
+done
