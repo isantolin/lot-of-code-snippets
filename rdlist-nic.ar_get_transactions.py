53187b5a17100efcdebe5165dc4cdeb96f47a98a
Ignacio Santolin <264640+isantolin@users.noreply.github.com>
2020-05-31 13:13:24 -0300

    Paralelized tasks

--- rdlist-nic.ar_get_transactions.py
+++ rdlist-nic.ar_get_transactions.py
@@ -1,11 +1,14 @@
 import requests
+from gevent import monkey
+
+monkey.patch_all(thread=False, select=False)
 from bs4 import BeautifulSoup
 import pandas as pd
 import sqlalchemy
-import urllib
 import re
-import http
-
+import requests
+import grequests
+import io
 
 request = requests.get('https://rdlist.nic.ar/rd_list/')
 soup = BeautifulSoup(request.text, 'html.parser')
@@ -13,38 +16,42 @@ mydivs = soup.findAll("a", {"href": re.compile(r'csv')})
 last_file = int(re.sub("[^0-9]", "", mydivs[0]['href']))
 
 lst = []
-i = 201
+init = 201
 DB_HOST = "localhost"
-DB_USER = "postgres"
+DB_USER = "root"
 DB_PASS = "_"
 DB_DB = "NicAr"
+urls = []
 
-while True:
-    try:
-        df_temp = pd.read_csv("https://rdlist.nic.ar/rd_list/download/" + str(i) + "/csv/",
-                              encoding='ISO-8859-1',
-                              header=0,
-                              delimiter=',',
-                              error_bad_lines=False,
-                              decimal=",",
-                              na_values='')
-        lst.append(df_temp)
-        del df_temp
-    except (urllib.error.HTTPError, urllib.error.URLError, ConnectionResetError, http.client.IncompleteRead) as err:
-        print(err)
-        if last_file < i:
-            break
-        else:
-            pass
-    print(i)
-    i = i + 1
+for i in range(201, last_file):
+    urls.append("https://rdlist.nic.ar/rd_list/download/" + str(i) + "/csv/")
+
+requests = (grequests.get(u) for u in urls)
+responses = grequests.map(requests, size=4)
+
+for response in responses:
+    if response is not None:
+        if 199 < response.status_code < 400:
+            print(response.headers['Content-Type'])
+            df_temp = pd.read_csv(io.BytesIO(response.content),
+                                  engine='python',
+                                  encoding='utf-8',
+                                  header=0,
+                                  delimiter=',',
+                                  error_bad_lines=False,
+                                  decimal=",",
+                                  na_values='')
+            lst.append(df_temp)
+            del df_temp
+    else:
+        print("Empty Response")
 
 df = pd.concat(lst, ignore_index=True)
 del lst
 df = df[~df.index.duplicated(keep='first')]
 df["fecha_registro"] = pd.to_datetime(df["fecha_registro"], infer_datetime_format=True)
 
-engine = sqlalchemy.create_engine("postgresql://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + '/' + DB_DB)
+engine = sqlalchemy.create_engine("mysql://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + '/' + DB_DB)
 
 dtype = {'tipo': sqlalchemy.types.CHAR(),
          'dominio': sqlalchemy.types.VARCHAR(length=170),
