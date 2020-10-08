49a458fb404eb01e2a7985660897bea74e160b62
Ignacio Santolin <264640+isantolin@users.noreply.github.com>
2020-10-08 00:22:46 -0300

    Added ApiGen

--- postinstall_multidist.sh
+++ postinstall_multidist.sh
@@ -158,6 +158,10 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
+wget https://github.com/ApiGen/ApiGen.github.io/raw/master/apigen.phar
+chmod +x apigen.phar
+sudo mv apigen.phar /usr/bin/apigen
+
 sudo pear channel-update pear.php.net
 sudo pear install PHP_CodeSniffer
 
