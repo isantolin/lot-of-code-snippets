f29256d2aacba35443fe041c8c403103c93f5c49
Ignacio Santolin <264640+isantolin@users.noreply.github.com>
2022-03-13 14:08:43 -0300

    NVIDIA: Removed binary Driver

--- postinstall_multidist.sh
+++ postinstall_multidist.sh
@@ -93,11 +93,6 @@ elif [ "$DIST" == "Fedora" ]; then
   sudo cp -p /usr/share/X11/xorg.conf.d/nvidia.conf /etc/X11/xorg.conf.d/nvidia.conf
   sudo dnf remove xorg-x11-drv-nouveau -y
   sudo dracut /boot/initramfs-$(uname -r).img $(uname -r) --force
-  wget https://us.download.nvidia.com/XFree86/Linux-x86_64/390.147/NVIDIA-Linux-x86_64-390.147.run
-  
-  # On next bootup will show console, to install NVIDIA-Linux-x86_64-*.*.run
-  sudo systemctl set-default multi-user.target
-  # following the install execute "sudo systemctl set-default graphical.target"
   
   # Performance Tweaks
   sudo grubby --update-kernel=ALL --args="processor.ignore_ppc=1 nowatchdog ipv6.disable=1"
