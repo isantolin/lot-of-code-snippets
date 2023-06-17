1304ca3c7ec3327c0e2d7a7fac4373d173600e63
Ignacio Santolin <264640+isantolin@users.noreply.github.com>
2023-06-17 11:53:06 -0300

    python: fixed character, added --ignore-installed

--- postinstall_multidist.sh
+++ postinstall_multidist.sh
@@ -114,7 +114,7 @@ sudo pip3 install pip wheel --upgrade --pre
 # Perl Upgrade
 sudo cpanm App::cpanoutdated
 
-sudo echo "pip --disable-pip-version-check list --outdated --pre --format=json | python -c \"import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))\" | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install --upgrade --pre -U" | sudo tee /usr/bin/auto-upgrade-ign.sh
+sudo echo "pip --disable-pip-version-check list --outdated --pre --format=json | python -c \"import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))\" | grep -v '^-e' | cut -d = -f 1  | xargs -n1 pip install --upgrade --pre --ignore-installed -U" | sudo tee /usr/bin/auto-upgrade-ign.sh
 sudo echo -e "dnf -y update\nflatpak update -y\ncpan-outdated -p | cpanm\nfwupdmgr get-devices\nfwupdmgr refresh --force\nfwupdmgr get-updates\nfwupdmgr update" >> /usr/bin/auto-upgrade-ign.sh
 chmod -x /usr/bin/auto-upgrade-ign.sh
 sudo sh /usr/bin/auto-upgrade-ign.sh
